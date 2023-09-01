41242065d4a4ef269810298dcfad81cd08274b5d
Leo Lomardo <80079076+LeoLomardo@users.noreply.github.com>
2023-08-31 21:24:45 -0300

    Update cat.c
    Adicionei a funcionalidade dele "printar" todo o conteúdo, de todos os arquivos passados por parâmetro

--- Lab1/cat.c
+++ Lab1/cat.c
@@ -3,20 +3,24 @@
 
 #define MAX_SIZE 200
 
+/*corrigir porque ele ta printando 2x cada linha do arquivo*/
+
 int main(int argc, char*argv[]){
-    FILE *arquivo;
-    arquivo = fopen(argv[1], "r");
+    for(int i =i; i<argc; i++){
+        FILE *arquivo;
+        arquivo = fopen(argv[i], "r");
+        
+        if(!arquivo){
+            printf("Erro ao abrir o arquivo");
+            exit(2);
+        }
     
-    if(!arquivo){
-        printf("Erro ao abrir o arquivo");
-        exit(2);
-    }
-
-    char buffer[MAX_SIZE];
-
-    while(!feof(arquivo)){
-        fscanf(arquivo, "%s", buffer);
-        printf("%s\n", buffer);
+        char buffer[MAX_SIZE];
+    
+        while(!feof(arquivo)){
+            fscanf(arquivo, "%s", buffer);
+            printf("%s\n", buffer);
+        }
     }
 
     return 0;
