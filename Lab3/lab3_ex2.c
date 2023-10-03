63b17ce0c92c11d1c514bbf380a6ba75acb4d117
STimmy10 <lucasdaibes@yahoo.com.br>
2023-10-03 20:33:57 -0300

    Final

--- Lab3/lab3_ex2.c
+++ Lab3/lab3_ex2.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-#define TAM_MAX 100
+#define TAM_MAX 10000
 #define NUM_THREADS 2
 #define TAM_PARTES TAM_MAX / NUM_THREADS
 
@@ -11,12 +11,11 @@ int vetorGeral[TAM_MAX];
 
 void *operaVet(void *threadID){
     int thread_id = *((int *)threadID);
-    int comeco  = thread_id * TAM_PARTES;
-    int fim = comeco + TAM_MAX;
+    int comeco = thread_id * TAM_PARTES;
+    int fim = comeco + TAM_PARTES;
 
     for(int i = comeco; i < fim; i++){
-        vetorGeral[i] *= 2;
-        vetorGeral[i] += 2;
+        vetorGeral[i] = 2*vetorGeral[i] + 2;
     }
     return NULL;
 }
@@ -30,32 +29,30 @@ int main(void){
     int thread_id[NUM_THREADS];
     int i;
     
-
     for(i = 0; i < NUM_THREADS; i++){
         thread_id[i] = i;
         pthread_create(&threads[i], NULL, operaVet, &thread_id[i]);
     }
 
-
     for(i = 0; i < NUM_THREADS; i++){
         pthread_join(threads[i], NULL);
     }
 
-    
     int igual = 1;  // Assumimos que todos os valores são iguais
+    
 
-    for (i = 1; i < TAM_MAX; i++) {
-        if (vetorGeral[i] != vetorGeral[i - 1]) {
+    for (i = 0; i < TAM_PARTES; i++) {
+        if (vetorGeral[i] != vetorGeral[i + TAM_PARTES]) {
             igual = 0;  // Se encontrarmos um valor diferente, definimos igual como 0
             break;      // Saímos do loop
         }
     }
-
     if (igual) {
         printf("Todos os valores do vetor são iguais.\n");
     } else {
         printf("Pelo menos um valor do vetor é diferente.\n");
     }
+    
 
     return 0;
 }
