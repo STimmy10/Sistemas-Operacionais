8d1f699d4738c2c78464c0d83486b2d7bc8c0e8e
STimmy10 <lucasdaibes@yahoo.com.br>
2023-10-27 23:50:24 -0300

    Typedef Comando ajustado

--- Trabalho1/interpretador.c
+++ Trabalho1/interpretador.c
@@ -9,14 +9,11 @@
 
 // Definir as estruturas dos objetos de comando
 typedef struct {
-    char* nome_programa; // Nome do programa
-    int momento_inicio; // Momento de início
-    int tempo_duracao; // Tempo de duração
-} Comando_RealTime;
-
-typedef struct {
-    char* nome_programa; // Nome do programa 
-} Comando_RoundRobin;
+    char nome_programa[50];
+    int momento_inicio;
+    int tempo_duracao;
+    int tipo; // 0 para RoundRobin, 1 para RealTime
+} Comando;
 
 
 int main() {
@@ -24,8 +21,9 @@ int main() {
     int segmento;
     char *p;
     char texto[100];
-    Comando_RealTime* comandos_realtime;
-    Comando_RoundRobin* comandos_roundrobin;
+    int num_comandos = 0;
+    char line[100];
+    Comando *comandos;
 
     // Cria a memória compartilhada com a chave 7000
     segmento = shmget(chave, 1024, IPC_CREAT);
@@ -35,18 +33,12 @@ int main() {
     }
 
     // Anexa a memória compartilhada ao processo
-    comandos_realtime = (Comando_RealTime *)shmat(segmento, NULL, 0);
-    if (comandos_realtime == (Comando_RealTime *)-1) {
+    comandos = (Comando *)shmat(segmento, NULL, 0);
+    if (comandos == (Comando *)-1) {
         perror("Erro ao anexar a memória compartilhada");
         exit(1);
     }
 
-    // Anexa a memória compartilhada ao processo
-    comandos_roundrobin = (Comando_RoundRobin *)shmat(segmento, NULL, 0);
-    if (comandos_roundrobin == (Comando_RoundRobin *)-1) {
-        perror("Erro ao anexar a memória compartilhada");
-        exit(1);
-    }
 
     FILE *arquivo = fopen("exec.txt", "r"); // Abrindo o arquivo em modo de leitura ("r")
 
@@ -55,31 +47,40 @@ int main() {
         return 1;
     }
 
-    char line[100];
-    int index_realtime = 0;
-    int index_roundrobin = 0;
-
     while (fgets(line, sizeof(line), arquivo) != NULL) {
         char comando[50];
 
         if (sscanf(line, "Run %s", comando) == 1) {
+            Comando novo_comando;
+            strcpy(novo_comando.nome_programa, comando);
+
             if (strstr(line, "I=") && strstr(line, "D=")) {
                 // É um comando RealTime
-                Comando_RealTime comando_realtime;
-                sscanf(line, "Run %s I=%d D=%d", comando, &comando_realtime.momento_inicio, &comando_realtime.tempo_duracao);
-                printf("Comando RealTime: %s I=%d D=%d\n", comando, comando_realtime.momento_inicio, comando_realtime.tempo_duracao);
-                index_realtime++;
+                int momento_inicio, tempo_duracao;
+                sscanf(line, "Run %*s I=%d D=%d", &momento_inicio, &tempo_duracao);
+                novo_comando.momento_inicio = momento_inicio;
+                novo_comando.tempo_duracao = tempo_duracao;
+                novo_comando.tipo = 1; // RealTime
             } else {
                 // É um comando RoundRobin
-                Comando_RoundRobin comando_roundrobin;
-                printf("Comando RoundRobin: %s\n", comando);
-                index_roundrobin++;
+                novo_comando.momento_inicio = 0; // Valor padrão para RoundRobin
+                novo_comando.tempo_duracao = 0; // Valor padrão para RoundRobin
+                novo_comando.tipo = 0; // RoundRobin
             }
-            
+
+            comandos[num_comandos] = novo_comando;
+            num_comandos++;
         }
     }
-    printf("Quantidade de comandos RealTime: %d\n", index_realtime);
-    printf("Quantidade de comandos RoundRobin: %d\n", index_roundrobin);
+
+    // Agora, você tem um array com todos os comandos (RealTime e RoundRobin)
+    for (int i = 0; i < num_comandos; i++) {
+        if (comandos[i].tipo == 1) {
+            printf("RealTime - Nome: %s, Início: %d, Duração: %d\n", comandos[i].nome_programa, comandos[i].momento_inicio, comandos[i].tempo_duracao);
+        } else {
+            printf("RoundRobin - Nome: %s\n", comandos[i].nome_programa);
+        }
+    }
 
     fclose(arquivo); // Fecha o arquivo quando terminar de usá-lo
 
