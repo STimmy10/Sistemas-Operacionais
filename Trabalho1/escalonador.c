ee777845f38aa7c5e150f7cfb9261f083b14583a
STimmy10 <lucasdaibes@yahoo.com.br>
2023-10-30 05:37:25 -0300

    Era pra estar certo, erro ta dentro do for

--- Trabalho1/escalonador.c
+++ Trabalho1/escalonador.c
@@ -25,11 +25,29 @@ void trataAlarme(int signum){
     rodando = 0;
 }
 
-sem_t fila_mutex; // Declare um semáforo para proteger a fila
+sem_t fila_mutex; // Semáforo para proteger a fila
+sem_t exec_mutex; // Semáforo para controlar a execução dos processos
 
 void inicializarFila(FilaComandos *fila) {
     fila->tamanho = 0;
     sem_init(&fila_mutex, 0, 1); // Inicializa o semáforo com um recurso
+    sem_init(&exec_mutex, 0, 1); // Inicializa o semáforo de execução com um recurso
+}
+
+void pararComando(Comando* comando) {
+    signal(comando->pid, SIGSTOP);
+}
+
+int conflitoEscalonamento(const Comando* novo, const FilaComandos* fila) {
+    for (int i = 0; i < fila->tamanho; i++) {
+        const Comando* existente = &fila->comandos[i];
+        if (existente->tipo == REAL_TIME && novo->tipo == REAL_TIME) {
+            if (novo->momento_inicio >= existente->momento_inicio && novo->momento_inicio < (existente->momento_inicio + existente->tempo_duracao)) {
+                return 1; // Conflito de tempo de início
+            }
+        }
+    }
+    return 0;
 }
 
 void adicionarComando(FilaComandos *fila, Comando comando) {
@@ -37,13 +55,17 @@ void adicionarComando(FilaComandos *fila, Comando comando) {
         printf("Comando RealTime ultrapassaria do minuto e não foi adicionado à fila.\n");
         return;
     }
-    
+    if (conflitoEscalonamento(&comando, fila)) {
+        printf("Conflito de escalonamento para o comando %s\n", comando.nome_programa);
+        return; 
+    }
     printf("Adicionando comando %s à fila de comandos\n", comando.nome_programa);
     comando.pid = fork();
     if (comando.pid == 0) {
         // Processo filho
         execve(comando.nome_programa, NULL, NULL);
         perror("Erro ao executar o comando");
+        sleep(1);
         signal(comando.pid, SIGSTOP);
         exit(1);
     } else if (comando.pid > 0) {
@@ -97,26 +119,40 @@ int main() {
         info_tempo = localtime(&tempo_atual);
         segundos_atuais = info_tempo->tm_sec;
 
+        int executando_real_time = 0;
+
         for (int i = 0; i < fila.tamanho; i++) {
+            time(&tempo_atual);
+            info_tempo = localtime(&tempo_atual);
+            segundos_atuais = info_tempo->tm_sec;
             if (fila.comandos[i].tipo == REAL_TIME) {
                 time(&tempo_atual);
-                int diferenca_tempo = fila.comandos[i].momento_inicio - tempo_atual;
-                if (diferenca_tempo <= 0) {
+                segundos_atuais = info_tempo->tm_sec;
+                int diferenca_tempo = fila.comandos[i].momento_inicio - segundos_atuais;
+                if (diferenca_tempo <= 0 && diferenca_tempo >= -1) {
+                    sem_wait(&exec_mutex); // Bloqueia o semáforo de execução
+                    printf("Segundo de início: %d - segundo atual: %d \n", fila.comandos[i].momento_inicio, segundos_atuais);
+                    printf("Diferença de tempo: %d\n", abs(diferenca_tempo));
                     // É hora de executar o comando RealTime
-                    comando_atual = &fila.comandos[i];
+                    executando_real_time = 1;
                     signal(fila.comandos[i].pid, SIGCONT);
                     sleep(fila.comandos[i].tempo_duracao);
                     signal(fila.comandos[i].pid, SIGSTOP);
+                    sem_post(&exec_mutex); // Libera o semáforo de execução
                     }
-            } else if (fila.comandos[i].tipo == ROUND_ROBIN) {
+            } else if (fila.comandos[i].tipo == ROUND_ROBIN && executando_real_time == 0) {
+                sem_wait(&exec_mutex); // Bloqueia o semáforo de execução
                 // Rodando os comandos RoundRobin
-                printf("Sem nada de maior prioridade, rodando round robin\n");
-                comando_atual = &fila.comandos[i];
                 signal(fila.comandos[i].pid, SIGCONT);
                 sleep(1);
                 signal(fila.comandos[i].pid, SIGSTOP);
+                sem_post(&exec_mutex); // Libera o semáforo de execução
             }
             sleep(1);
+            for(int j = 0; j < fila.tamanho; j++){
+                pararComando(&fila.comandos[j]);
+            }
+            executando_real_time = 0;
         }
 
     }
