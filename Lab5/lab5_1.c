cde68762c1cdacb0fa5ea1cfd7d19c424f7ee60d
Leo Lomardo <leoland771@gmail.com>
2023-11-17 17:55:49 -0300

    exercio_1 feito
    Exeercicio 1 feito, falta o 2 e o relatorio

---
+++ Lab5/lab5_1.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>   //adiciona flags adicionais para mmap
+#include <fcntl.h>      //define O_CREATE
+#include <semaphore.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#define NUM_MENS 128
+#define BUFFER_SIZE 2048
+#define SEM_NAME1 "/sem1_name"
+#define SEM_NAME2 "/sem2_name"
+#define SEM_GERAL "shared_sem"
+
+
+int main(void){
+    sem_t *sem1;
+    sem_t *sem2;
+    int sem_Geral;
+    void *sharedMem_ptr;
+    int count = 0;
+
+    sem_Geral = shm_open(SEM_GERAL, O_CREAT | O_RDWR, 0666);
+    /*
+        FLAG O_RDWR utilizada para abrir a regiao com permissão de leitura e de escrita
+    */
+    if(sem_Geral == -1){
+        perror("sem_open");
+        exit(1);
+    }
+    if (ftruncate(sem_Geral, BUFFER_SIZE) == -1) {
+        perror("ftruncate");
+        exit(1);
+    }
+    sem1 = sem_open(SEM_NAME1, O_CREAT, 0666, 0);
+    if (sem1 == SEM_FAILED) {
+        perror("sem_open1");
+        exit(1);
+    }
+
+    sem2 = sem_open(SEM_NAME2, O_CREAT, 0666, 0);
+    if (sem2 == SEM_FAILED) {
+        perror("sem_open2");
+        exit(1);
+    }
+
+    sharedMem_ptr = mmap(NULL, BUFFER_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, sem_Geral,0 );
+
+    pid_t pid = fork();
+
+    if(pid == -1){
+        perror("Process Error");
+        exit(1);
+    }else if(pid ==0){ //processo filho
+
+        for(count =  0; count < NUM_MENS; count++){
+            sem_wait(sem1);
+            printf("\t[FILHO]LEITURA> %s  \n",(char*)sharedMem_ptr);
+            //fflush(stdout); 
+            sem_post(sem2);
+        }
+    }else{
+        for(count =  0; count < NUM_MENS ; count++){
+            sprintf((char *) sharedMem_ptr, "Mensagem %d", count+1);
+            printf("[PAI]ESCRITA> %s\n",(char*)sharedMem_ptr);
+            //fflush(stdout); 
+            
+            sem_post(sem1);
+            sem_wait(sem2);
+        }
+    }   
+    sem_close(sem1);
+    sem_unlink(SEM_NAME1);
+
+    sem_close(sem2);
+    sem_unlink(SEM_NAME2);
+
+    close(sem_Geral);
+
+    munmap(sharedMem_ptr, BUFFER_SIZE); //Libera memoria compartilhada alocada
+
+    return 0;
+}
+
+
+//LINKS DE CONSULTA
+/*
+    https://stackoverflow.com/questions/21311080/linux-shared-memory-shmget-vs-mmap#:~:text=mmap%20method%20is%20a%20little,would%20suggest%20going%20with%20that.
+    https://man7.org/linux/man-pages/man3/shm_open.3.html    
+    https://stackoverflow.com/questions/68741707/what-is-the-different-of-using-fflushstdout-and-not-using-it
+*/
